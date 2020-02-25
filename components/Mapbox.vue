df449b4c49f20436cf88010bc735482b265c6b9a
koba1108 <paseli.koba@gmail.com>
2020-02-25 18:02:00 +0900

    add map

---
+++ components/Mapbox.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    {{ MglMap }}
+    <mgl-map :accessToken="accessToken" :mapStyle="mapStyle"/>
+  </div>
+</template>
+
+<script>
+  import MapboxGeoCoder from '@mapbox/mapbox-gl-geocoder'
+  import Mapbox from 'mapbox-gl'
+  import { MglMap } from 'vue-mapbox'
+
+  const ACCESS_TOKEN = 'pk.eyJ1IjoieWtvYmEiLCJhIjoiY2s3MWxrZngyMDcxYzNlcXM4ZnVzbTRoMiJ9.pW6fXCV53omjfT36kpEdOA'
+  const MAP_STYLE = 'mapbox://styles/mapbox/streets-v11'
+  export default {
+    components: {
+      MglMap,
+    },
+    data() {
+      return {
+        mapbox: null,
+        MglMap: MglMap,
+        accessToken: ACCESS_TOKEN,
+        mapStyle: MAP_STYLE,
+      }
+    },
+    created() {
+      this.mapbox = Mapbox
+      /*
+      this.mapbox.addControl(
+        new MapboxGeoCoder({
+          accessToken: ACCESS_TOKEN,
+          mapboxgl: Mapbox
+        })
+      )
+       */
+    },
+  }
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -12,16 +12,13 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: pkg.description },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      {
-        rel: 'stylesheet',
-        href:
-          'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
-      }
-    ]
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' },
+      { rel: 'stylesheet', href: 'https://api.mapbox.com/mapbox-gl-js/v1.8.0/mapbox-gl.css' },
+    ],
   },
 
   /*
@@ -33,21 +30,20 @@ export default {
   ** Global CSS
   */
   css: [
-    '~/assets/style/app.styl'
+    '~/assets/style/app.styl',
   ],
 
   /*
   ** Plugins to load before mounting the App
   */
   plugins: [
-    '@/plugins/vuetify'
+    '@/plugins/vuetify',
   ],
 
   /*
   ** Nuxt.js modules
   */
-  modules: [
-  ],
+  modules: [],
 
   /*
   ** Build configuration
@@ -57,13 +53,12 @@ export default {
     plugins: [new VuetifyLoaderPlugin()],
     loaders: {
       stylus: {
-        import: ['~assets/style/variables.styl']
-      }
+        import: ['~assets/style/variables.styl'],
+      },
     },
     /*
     ** You can extend webpack config here
     */
-    extend(config, ctx) {
-    }
-  }
+    extend(config, ctx) {},
+  },
 }
